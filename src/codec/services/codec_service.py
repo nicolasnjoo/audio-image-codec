774588d81517da137ed70bd54cd79c32886ac0fa
MrWaffleMaker <nnjoo@hotmail.com>
2025-12-16 18:37:19 -0500

    Make package importable and add smoke test

---
+++ src/codec/services/codec_service.py
@@ -0,0 +1,6 @@
+class CodecService:
+    def encode(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def decode(self, *args, **kwargs):
+        raise NotImplementedError
---
+++ tests/test_smoke.py
@@ -0,0 +1,2 @@
+def test_import():
+    import codec  # noqa: F401
